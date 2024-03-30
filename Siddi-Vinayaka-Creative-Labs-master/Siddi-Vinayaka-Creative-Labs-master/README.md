9a7191b1efd7629a3fc7ce3bd844e61f96883193
Sonia009 <134524881+Sonia09-coder@users.noreply.github.com>
2024-03-30 10:06:07 +0530

    News-today

---
+++ Siddi-Vinayaka-Creative-Labs-master/Siddi-Vinayaka-Creative-Labs-master/README.md
@@ -0,0 +1,10 @@
+# React + Vite
+
+1. To run this : In terminal of this project
+2. Write npm i
+3. npm i fromik
+4. npm i react-router-dom
+5. npm install @mui/material @emotion/react @emotion/styled
+6. npm install @mui/material @mui/styled-engine-sc styled-components
+7. npm install @mui/icons-material
+8. Use admin123@gmail.com as emailid to navigate to admin homepage
---
+++ Siddi-Vinayaka-Creative-Labs-master/Siddi-Vinayaka-Creative-Labs-master/src/AdminNav/AdminNav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './AdminNav.css'
+import { useNavigate } from 'react-router-dom'
+
+const AdminNav = () => {
+    const navigate=useNavigate()
+
+    return (
+      <div className="NewNav">
+      
+      <ul>
+      <li>
+    <a className="home" onClick={() => navigate('/adminhome')}>Home</a>
+     </li>
+          <li className="logout" onClick={()=>navigate('/login')}><a>Logout</a></li>
+      </ul>
+     </div>
+    );
+}
+
+export default AdminNav
